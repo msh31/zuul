645bfabc0b730827b6843598922344eda5f2334b
Marco <mshendriks03@gmail.com>
2025-02-24 21:42:42 +0100

    feat: app properties

--- Program.cs
+++ Program.cs
@@ -2,6 +2,10 @@ class Program
 {
 	public static void Main(string[] args)
 	{
+		//Game properties
+		Console.Title = "Zuul";
+		Console.ForegroundColor = ConsoleColor.Gray;
+		
 		// Create and play the Game.
 		Game game = new Game();
 		game.Play();
